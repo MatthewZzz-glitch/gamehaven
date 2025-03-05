854985b92ef59dada8bc144d4debfa88f2a80017
gamehaven <31mhan@stratfordschools.net>
2025-03-05 15:26:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const games = document.querySelectorAll('.game');
+    
+    games.forEach(game => {
+        game.addEventListener('click', () => {
+            const url = game.getAttribute('data-url');
+            window.location.href = url;
+        });
+    });
+});
 
